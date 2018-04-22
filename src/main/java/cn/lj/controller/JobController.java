227dca0e29984b721e0ed6bc4f24e53e89f1d38c
lj <hfutlj@163.com>
2018-04-22 18:11:36 +0800

    no message

--- src/main/java/cn/lj/controller/JobController.java
+++ src/main/java/cn/lj/controller/JobController.java
@@ -4,7 +4,9 @@ import org.quartz.*;
 import org.quartz.impl.matchers.GroupMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,25 +50,38 @@ public class JobController {
         return jobList;
     }
 
+    /**
+     * 添加新Job
+     */
     @RequestMapping("/addJob")
-    public void addJob() {
-        JobBuilder jobBuilder = JobBuilder.newJob(MyJob.class);
-        JobDetail jobDetail = jobBuilder.withIdentity("testJob").build();
+    public void addJob(@RequestParam String jobName, @RequestParam String jobClassPath, String jobGroup, @RequestParam String cronExpression, @RequestParam String triggerName, String TriggerGroup) {
+        // 根据Job执行类的全路径获取执行类
+        Class<? extends Job> jobClass;
+        try {
+            jobClass = (Class<? extends Job>) ClassLoader.getSystemClassLoader().loadClass(jobClassPath);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return;
+        }
+        //
+        JobBuilder jobBuilder = JobBuilder.newJob(jobClass);
+        jobGroup = StringUtils.isEmpty(jobGroup) ? "DEFAULT" : jobGroup;
+        JobDetail jobDetail = jobBuilder.withIdentity(jobName, jobGroup).build();
         TriggerBuilder triggerBuilder = TriggerBuilder.newTrigger();
         CronTrigger trigger = (CronTrigger) triggerBuilder
-                                    .withIdentity("testTrigger")
-                                    .startNow()
-                                    .withSchedule(CronScheduleBuilder.cronSchedule("0/3 * * * * ?"))
-                                    .build();
+                .withIdentity(triggerName,TriggerGroup)
+                .startNow()
+                .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression))
+                .build();
         try {
-            scheduler.scheduleJob(jobDetail,trigger);
+            scheduler.scheduleJob(jobDetail, trigger);
         } catch (SchedulerException e) {
             e.printStackTrace();
         }
     }
 
     @RequestMapping("/runOnce")
-    public void runOnce(){
+    public void runOnce() {
         try {
             scheduler.triggerJob(JobKey.jobKey("testJob"));
         } catch (SchedulerException e) {
